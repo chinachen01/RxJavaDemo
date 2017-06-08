856da6c01fe9407773776a717453674f837d8052
Yong.Chen <chinachen01@163.com>
2017-06-08 18:17:02 +0800

    chore:add some observable.

--- app/src/main/java/com/focus/rxjavademo/MyApp.java
+++ app/src/main/java/com/focus/rxjavademo/MyApp.java
@@ -2,6 +2,11 @@ package com.focus.rxjavademo;
 
 import android.app.Application;
 
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.FormatStrategy;
+import com.orhanobut.logger.Logger;
+import com.orhanobut.logger.PrettyFormatStrategy;
+
 /**
  * Created by focus on 17/6/7.
  */
@@ -10,6 +15,15 @@ public class MyApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        FormatStrategy formatStrategy = PrettyFormatStrategy.newBuilder()
+                .tag("RX_ANDROID")   // (Optional) Global tag for every log. Default PRETTY_LOGGER
+                .build();
 
+        Logger.addLogAdapter(new AndroidLogAdapter(formatStrategy) {
+            @Override
+            public boolean isLoggable(int priority, String tag) {
+                return BuildConfig.DEBUG;
+            }
+        });
     }
 }
--- app/src/main/java/com/focus/rxjavademo/view/ObservableCreateActivity.java
+++ app/src/main/java/com/focus/rxjavademo/view/ObservableCreateActivity.java
@@ -3,13 +3,19 @@ package com.focus.rxjavademo.view;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.focus.rxjavademo.R;
+import com.orhanobut.logger.Logger;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
+
 
 /**
  * Created by focus on 17/6/7.
@@ -28,20 +34,57 @@ public class ObservableCreateActivity extends AppCompatActivity {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.button_just:
-
+                just();
+                break;
+            case R.id.button_from:
+                from();
+                break;
+            case R.id.button_create:
+                create();
+                break;
+            case R.id.button_range:
+                range();
+                break;
+            case R.id.button_interval:
+                interval();
                 break;
             default:
         }
+    }
+    private Disposable mInterval;
+    private void interval() {
+        mInterval = Observable.interval(0, 1, TimeUnit.SECONDS)
+                .subscribe(aLong -> Logger.d("interval long: " + aLong));
+    }
+
+    private void range() {
+        mInterval.dispose();
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6);
+        Observable.range(0, list.size())
+                .subscribe(i -> Logger.d("range number: %d", list.get(i)));
     }
 
     private void just() {
         Observable.just("hello android")
                 .subscribe(s -> {
-                    Log.d(TAG, "just character: " + s);
-                    Log.d(TAG, "");
+                    Logger.d("just character: %s", s);
                 });
         Observable.just(1, 2, 3, 4, 5, 6, 7)
-                .subscribe( i -> Log.d(TAG, "just number arrays: " + i));
-
+                .subscribe(i -> Logger.d("just number: %d", i));
     }
+
+    private void from() {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6);
+        Observable.fromIterable(list)
+                .subscribe(i -> Logger.d("from number: %d", i));
+    }
+
+    private void create() {
+        Observable.create(emitter -> {
+            emitter.onNext("hello");
+            emitter.onNext("world");
+            emitter.onComplete();
+        }).subscribe(s -> Logger.d("create string: %d" + s));
+    }
+
 }
